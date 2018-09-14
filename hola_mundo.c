58ce4aced20eda2763674870ec260410b66137a5
Julián Alfonso Cillero Martín <41955270+julianmaux@users.noreply.github.com>
2018-09-14 16:59:55 +0200

    Hola mundo mejorado
    Programa hola mundo que te pide tu nombre para realizar un saludo

--- hola_mundo.c
+++ hola_mundo.c
@@ -5,6 +5,9 @@
 
 int main(int argc, char *argv[]){
 
-	printf("Hola mundo\n");
+	char s[20];
+	printf("Buenos días ¿cómo te llamas?:");
+	scanf("%s",s);
+	printf("Es un placer saludarte %s \n",s);
 	return 0;
 }
