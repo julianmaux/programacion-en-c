4053cd347d59bf5689d27e6076171dec5c0a9ba3
Julián Alfonso Cillero Martín <41955270+julianmaux@users.noreply.github.com>
2019-09-16 20:15:36 +0200

    Update hola_mundo.c
    Esta es una modificación de mi archivo

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,11 +1,11 @@
 /* Primer programa del curso
 	
 */
-#include <stdio.h>
+#include <stdio.h> //directivas del preprocesador
 
 int main(int argc, char *argv[]){
 
-	char s[20];
+	char s[20]; //cadena de 20 caracteres
 	printf("Buenos días ¿cómo te llamas?:");
 	scanf("%s",s);
 	printf("Es un placer saludarte %s \n",s);
